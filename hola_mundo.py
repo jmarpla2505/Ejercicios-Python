14f70199202f9d10d54c70f21132a439e728458e
José Ángel <al_jmartosa@iessierradelasvillas.org>
2022-09-29 10:03:11 +0200

    Comentarios, entrada, salida de datos y tipos de datos en python

---
+++ hola_mundo.py
@@ -0,0 +1,22 @@
+#Tipos de datos y variables
+
+#Numéricos
+#enteros -- int
+#real -- float
+#Cadena de caracteres -- str
+#Lógico -- bool
+
+nombre = "José Ángel"
+edad = 17
+mayorOmenorEdad = False 
+
+#Entrada y salida de datos
+#Salida con print()
+print("Buenos días", nombre,"tu edad es", edad)
+
+#Entrada de datos -- input()
+nombre = input("Dime tu nombre:\n ")
+
+edad = input("Dime tu edad:\n ")
+
+print("Buenos días", nombre,"tu edad es", edad)
