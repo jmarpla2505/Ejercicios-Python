70c1d945fc05592c61f8f59168c1a048a7f52a32
José Ángel <al_jmartosa@iessierradelasvillas.org>
2022-10-05 22:51:05 +0200

    Terminado reto

--- retospiderman.py
+++ retospiderman.py
@@ -8,9 +8,30 @@ distancia1 = int(input("Dime la distancia al punto 1:\n "))
 distancia2 = int(input("Dime la distancia al punto 2:\n "))
 
 if abs(distancia1)>abs(distancia2):
+    if (distancia1>0 and distancia2>0):
 
-    print("El mejor camino es ir primero al punto 2 y luego al punto 1")
-    print("La distancia total es",distancia2*2+distancia1)
+      print("El mejor camino es ir primero al punto 2 y luego al punto 1")
+      print("La distancia total es",abs(distancia1))
+    else:
+        if distancia1<0 and distancia2<0:
+         print("El mejor camino es ir primero al punto 2 y luego al punto 1")
+         print("La distancia total es",abs(distancia1))
+        else:
+            print("El mejor camino es ir primero al punto 2 y luego al punto 1")
+            print("La distancia total es",abs(distancia1)+abs(distancia2)*2)
+
+
 else:
-    print("El mejor camino es ir primero al punto 1 y luego al punto 2")
-    print("La distancia total es",distancia1*2+distancia2)
+      if (distancia1>0 and distancia2>0):
+
+       print("El mejor camino es ir primero al punto 1 y luego al punto 2")
+       print("La distancia total es",abs(distancia2))
+      else:
+         if distancia1<0 and distancia2<0:
+           print("El mejor camino es ir primero al punto 1 y luego al punto 2")
+           print("La distancia total es",abs(distancia2)) 
+         else:
+           print("El mejor camino es ir primero al punto 1 y luego al punto 2") 
+           print("La distancia total es",abs(distancia1)*2+abs(distancia2)) 
+
+ 
