e8aab281baa7f7fef0c808fafa1a22bc4b7eac73
José Ángel <al_jmartosa@iessierradelasvillas.org>
2022-10-11 14:37:46 +0200

    Añadido ejercicio palabra secreta

---
+++ palabrasecreta.py
@@ -0,0 +1,9 @@
+
+
+palabrasecreta = "juan"
+
+ 
+while(palabrasecreta!="casa"):
+    palabrasecreta = input("Dime la contraseña:")
+    if (palabrasecreta == "casa"): 
+        print("Enhorabuena, has adivinado la contraseña")
