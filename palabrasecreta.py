c7664c4edd30bf6f6fd3220433b2aff6f94835e8
José Ángel <al_jmartosa@iessierradelasvillas.org>
2022-10-11 23:30:27 +0200

    Añadido limite de intentos

--- palabrasecreta.py
+++ palabrasecreta.py
@@ -1,9 +1,14 @@
 
 
 palabrasecreta = "juan"
-
+i = 0
  
 while(palabrasecreta!="casa"):
     palabrasecreta = input("Dime la contraseña:")
-    if (palabrasecreta == "casa"): 
-        print("Enhorabuena, has adivinado la contraseña")
+    i=i+1
+    if i>3:
+        print("Te has quedado sin intentos")
+        exit()
+if (palabrasecreta == "casa"): 
+ print("Enhorabuena, has adivinado la contraseña")
+
