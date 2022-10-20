6c231e2f162c59fb98a2505d4a64aa4a3021d7e7
José Ángel <al_jmartosa@iessierradelasvillas.org>
2022-10-20 10:15:00 +0200

    Añadido funciones

--- funciones.py
+++ funciones.py
@@ -1,8 +1,20 @@
-
-
 #Funciones definidas por el usuario
 
+from pytube import YouTube
+"""""
 def numerosenteros(numero1:int, numero2:int):
-    print(numero1+numero2)
+    #print("La suma es", numero1+numero2)
+    return numero1+numero2
 
-numerosenteros(1,2)
+
+suma = numerosenteros(1,2)
+print("La suma es:", suma)
+"""
+def descargaCancion(url:str):
+    youtube = YouTube(url)
+    print(youtube.author)
+    print("Descargando", youtube.title)
+    cancion = youtube.streams.get_audio_only()
+    cancion.download()
+
+descargaCancion("https://www.youtube.com/watch?v=VL1r5KOE6nc")
