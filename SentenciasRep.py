599e1a1820c660e230e18dd4990a2fcfaf01f684
José Ángel <al_jmartosa@iessierradelasvillas.org>
2022-10-24 11:16:34 +0200

    añadido sentenciasrp

---
+++ SentenciasRep.py
@@ -0,0 +1,14 @@
+#while
+"""""
+contador = 0
+while contador <=10:
+    print(contador)
+    contador += 1
+"""""
+#Bucle for
+#lista = [1,2,3,4,5,6,7,8,9,10]
+
+vNombres = ["Angel", "Maria", "Pepe", "Jose"]
+
+for num in vNombres:
+    print(num)
