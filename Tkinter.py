a24afda95a8a8c70d7b40c3128146023aa89cc20
José Ángel <al_jmartosa@iessierradelasvillas.org>
2022-11-21 11:13:58 +0100

    Explicación Tkinter

--- Tkinter.py
+++ Tkinter.py
@@ -1,22 +1,43 @@
 from tkinter import *
 from tkinter import ttk
 
+def saludar():
+    texto = campoTexto.get()
+    print(texto)
+    textoLabel.set(texto)
+
+
 #Generar la ventana
 ventana = Tk()
 ventana.title("Primer Ejercicio")
 ventana.geometry("300x400")
 ventana.resizable(width=False,height=False)
 ventana.config(background="light blue")
-ventana.iconbitmap("icon.png")
 
 #Generar el lienzo para pintar componentes
-frm = ttk.Frame(ventana, ).pack()
+frm = ttk.Frame(ventana)
+frm.pack()
 
 
 #Componentes Label y Button
-labelTexto = ttk.Label(frm, text="Hola Tkinter")
-labelTexto.config(background="red", border=5, font=("Arial", 15))
+textoLabel = StringVar()
+textoLabel.set("Hola Tkinter")
+labelTexto = ttk.Label(frm, textvariable=textoLabel)
+labelTexto.config(background="sky blue", border=5, font=("Arial", 15))
 labelTexto.pack()
+
+#Componente cuadro de texto
+campoTexto =ttk.Entry(frm)
+campoTexto.pack()
+
+
+ttk.Button(frm, text="Saludo", command=saludar).pack()
+
 ttk.Button(frm, text="Salir", command=ventana.destroy).pack()
+
+
+
+
+
 
 ventana.mainloop()
