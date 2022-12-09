82f8705dad63a51afb4c01b1ee3fab735697f062
José Ángel <al_jmartosa@iessierradelasvillas.org>
2022-12-09 11:09:45 +0100

    Mas funciones en flet y empezar proyecto frutería

--- proyectofruteria.py
+++ proyectofruteria.py
@@ -1,19 +1,20 @@
 import flet as ft
 
 def main(page:ft.Page):
-    page.title = ":-)"
+    page.title = "Aplicación frutería"
 
     def cambiarColor(e):
         t.value = textField_Nombre.value
 
     #Componente texto
-    t=ft.Text(value="Introducción a Flet", color="red", size=20)
+    t=ft.Text(value="Bienvenido", color="green", size=30)
     page.add(t)
     #Componente boton
     bt = ft.FloatingActionButton(icon=ft.icons.ADD, on_click=cambiarColor)
     page.add(bt)
 
-    
+    label_Nombre = "Dime tu nombre:"
+    page.add(label_Nombre)
     textField_Nombre = ft.TextField(label="Nombre", hint_text="Escribe tu nombre")
     #page.add(textField_Nombre)
 
